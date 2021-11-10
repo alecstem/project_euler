6e24bb78bdce634b496735f927e5c27d3542cc09
Alec <78441978+alecstem@users.noreply.github.com>
2021-11-10 17:47:01 -0500

    Create eratosthenesSieve.cpp

---
+++ helper_functions/eratosthenesSieve.cpp
@@ -0,0 +1,12 @@
+const int MAX_PR = 2'000'000;
+bitset<MAX_PR> isprime;
+vector<ll> eratosthenesSieve(ll lim) 
+{
+	isprime.set(); isprime[0] = isprime[1] = 0;
+	for (ll i = 4; i < lim; i += 2) isprime[i] = 0;
+		for (ll i = 3; i*i < lim; i += 2) if (isprime[i])
+			for (ll j = i*i; j < lim; j += i*2) isprime[j] = 0;
+	vector<ll> pr;
+	for (ll i = 2; i < lim; ++i) if (isprime[i]) pr.push_back(i);
+	return pr;
+}
